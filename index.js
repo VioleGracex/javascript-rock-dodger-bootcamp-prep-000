cc2f8218b5d548e2988ca38e9fd0eb6de383160f
Viole Grace <highdevilxx@hotmail.com>
2018-08-20 12:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,19 +78,25 @@ function createRock(x) {
   // Hmmm, why would we have used `var` here?
   var top = 0
 
-  rock.style.top = top
+  rock.style.top = `${top}px`
 
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+ GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock() {
+  function moveRock() 
+  {
+    var check = checkCollision(rock)
+   if (check === true) 
+   {
+     endGame()
+   }
     // implement me!
     // (use the comments below to guide you!)
     /**
