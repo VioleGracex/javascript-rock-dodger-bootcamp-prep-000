26197e526f7862bab793b03570b7b4ce0f965750
Viole Grace <highdevilxx@hotmail.com>
2018-08-20 12:55:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ var gameInterval = null
  * but all of your work should happen below.
  */
 
-function checkCollision(rock) {
+function checkCollision(rock)
+{
   // implement me!
   // use the comments below to guide you!
   const top = positionToInteger(rock.style.top)
@@ -25,18 +26,19 @@ function checkCollision(rock) {
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
-  if (top > 360) {
+  if (top > 360)
+  {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge+40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = dodgerLeftEdge+20;
 
-    if (false /**
+  /*  if (false /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
                * 1. The rock's left edge is < the DODGER's left edge,
@@ -45,9 +47,21 @@ function checkCollision(rock) {
                *    and the rock's right edge is < the DODGER's right edge;
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
-               */) {
-      return true
-    }
+               /) {*/
+               if (dodgerLeftEdge>=rockLeftEdge&&dodgerLeftEdge=<rockRightEdge)
+               {
+                 return true;
+               }
+              else if(dodgerLeftEdge<=rockLeftEdge&&dodgerRightEdge>=rockRightEdge)
+               {
+                 return true;
+               }
+               else if(dodgerRightEdge>=rockLeftEdge&&dodgerRightEdge=<rockRightEdge)
+               {
+                 return true;
+               }
+
+    //}
   }
 }
 
